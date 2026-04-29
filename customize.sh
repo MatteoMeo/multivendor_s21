8409ed004c07c83b8916ad3472ea06ac2424e27d
MatteoMeo <104899043+MatteoMeo@users.noreply.github.com>
2026-04-29 19:06:02 +0200

    Fix path formatting in customize.sh

--- customize.sh
+++ customize.sh
@@ -1,12 +1,12 @@
 rm -rf /vendor/tee
 rm -rf /vendor/firmware
 rm -rf /vendor/overlay
-mkdir -p "/vendor/tee"
-SET_METADATA "vendor" "tee" 0 2000 755 "u:object_r:tee_file:s0"
-mkdir -p "/vendor/firmware"
-SET_METADATA "vendor" "firmware" 0 2000 755 "u:object_r:vendor_firmware_file:s0"
-mkdir -p "/vendor/overlay"
-SET_METADATA "vendor" "overlay" 0 2000 755 "u:object_r:vendor_overlay_file:s0"
+mkdir -p /vendor/tee
+SET_METADATA vendor/tee 0 2000 755 "u:object_r:tee_file:s0"
+mkdir -p /vendor/firmware
+SET_METADATA vendor/firmware 0 2000 755 "u:object_r:vendor_firmware_file:s0"
+mkdir -p /vendor/overlay
+SET_METADATA vendor/overlay 0 2000 755 "u:object_r:vendor_overlay_file:s0"
 
 echo "(allow init_30_0 tee_file (dir (mounton)))" >> "/vendor/etc/selinux/vendor_sepolicy.cil"
 echo "(allow priv_app_30_0 tee_file (dir (getattr)))" >> "/vendor/etc/selinux/vendor_sepolicy.cil"
