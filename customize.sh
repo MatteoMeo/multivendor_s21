3a280f1e3d5b4efeca77d973248144cc5877cae3
MatteoMeo <104899043+MatteoMeo@users.noreply.github.com>
2026-04-29 19:04:34 +0200

    Update customize.sh
    Removed UN1CA builder correlations as i do things manually and this is only needed to remind me steps to do.
    Added Overlay and Firmware empty folders and related lines for cil file as i need them too for S21 Korean and Global.
    Other small changes...

--- customize.sh
+++ customize.sh
@@ -1,14 +1,19 @@
-DELETE_FROM_WORK_DIR "vendor" "tee"
-mkdir -p "$WORK_DIR/vendor/tee"
+rm -rf /vendor/tee
+rm -rf /vendor/firmware
+rm -rf /vendor/overlay
+mkdir -p "/vendor/tee"
 SET_METADATA "vendor" "tee" 0 2000 755 "u:object_r:tee_file:s0"
+mkdir -p "/vendor/firmware"
+SET_METADATA "vendor" "firmware" 0 2000 755 "u:object_r:vendor_firmware_file:s0"
+mkdir -p "/vendor/overlay"
+SET_METADATA "vendor" "overlay" 0 2000 755 "u:object_r:vendor_overlay_file:s0"
 
-if ! grep -q "tee_file (dir (mounton" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
-    echo "(allow init_202404 tee_file (dir (mounton)))" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
-    echo "(allow priv_app_202404 tee_file (dir (getattr)))" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
-fi
+echo "(allow init_30_0 tee_file (dir (mounton)))" >> "/vendor/etc/selinux/vendor_sepolicy.cil"
+echo "(allow priv_app_30_0 tee_file (dir (getattr)))" >> "/vendor/etc/selinux/vendor_sepolicy.cil"
+echo "(allow init_30_0 vendor_firmware_file (dir (mounton)))" >> "/vendor/etc/selinux/vendor_sepolicy.cil"
+echo "(allow priv_app_30_0 vendor_firmware_file (dir (getattr)))" >> "/vendor/etc/selinux/vendor_sepolicy.cil"
+echo "(allow init_30_0 vendor_overlay_file (dir (mounton)))" >> "/vendor/etc/selinux/vendor_sepolicy.cil"
+echo "(allow priv_app_30_0 vendor_overlay_file (dir (getattr)))" >> "/vendor/etc/selinux/vendor_sepolicy.cil"
 
-SET_PROP "vendor" "ro.vendor.multitee.supported_bootloaders" "$(cat "$(dirname ${BASH_SOURCE[0]})/supported_bootloaders" | tr '\n' ',' | sed 's/,$//')"
-SET_PROP "vendor" "ro.vendor.multitee.version" "$(cat "$(dirname ${BASH_SOURCE[0]})/module.prop" | grep 'version=' | sed 's/version=//')"
-if git -C "$(dirname ${BASH_SOURCE[0]})" rev-parse --is-inside-work-tree &>/dev/null; then
-    SET_PROP "vendor" "ro.vendor.multitee.revision" "$(git -C "$(dirname ${BASH_SOURCE[0]})" rev-parse --short=8 HEAD)"
-fi
+SET_PROP "vendor" "ro.vendor.multivendor.supported_bootloaders" "$(cat "$(dirname ${BASH_SOURCE[0]})/supported_bootloaders" | tr '\n' ',' | sed 's/,$//')"
+SET_PROP "vendor" "ro.vendor.multivendor.version" "$(cat "$(dirname ${BASH_SOURCE[0]})/module.prop" | grep 'version=' | sed 's/version=//')"
